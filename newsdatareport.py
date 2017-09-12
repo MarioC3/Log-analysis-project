4a3cbe163f62c9428e25d5d5c8c945c34f3addec
Mario A. Carlos <me@macarlos.com>
2017-09-11 22:51:26 -0600

    Review 1: Refactor connect database and execute query in one function to re-use it. Switch to only one query in function error day.

--- newsdatareport.py
+++ newsdatareport.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 '''
 News Data Report
     Program made by Beto Carlos for Udacity's FSND
@@ -10,23 +12,33 @@ Python program that returns 3 reports from the newsdata.sql database:
 import psycopg2
 
 
+# Connects to the database and executes the function.
+def query_exec(query):
+    '''
+    Function to connect to the database and execute the query
+    '''
+    try:
+        conn = psycopg2.connect("dbname=news")
+        cursor = conn.cursor()
+    except:
+        print("Couldn't connect to Database")
+    cursor.execute(query)
+    result = cursor.fetchall()
+    conn.close()
+
+    return result
+
+
 # 1. What are the most popular three articles of all time?
 def reporting_3_popular():
     '''
     Function that connects to the newsdata sql database and returns
     the most popular 3 articles of all time.
     '''
-    # Connects to database news
-    conn = psycopg2.connect("dbname=news")
-    cursor = conn.cursor()
     # To execute the query you will need to create the view 'popular3'
     # (See README - Notes 1)
-    cursor.execute('select * from popular3;')
-    result_3_popular = cursor.fetchall()
-    conn.close()
-
-    # Loops over the dict and prints the most popular books
-    for articles in result_3_popular:
+    # Loops over the dict to show the 3 most popular articles
+    for articles in query_exec('select * from popular3;'):
         print('Article: "{}" with {} views'.format(articles[1], articles[0]))
 
 
@@ -36,18 +48,10 @@ def reporting_popular_authors():
     Function that connects to the newsdata sql database and returns
     the authors in popularity descending order (views).
     '''
-    # Connects to database news
-    conn = psycopg2.connect("dbname=news")
-    cursor = conn.cursor()
-
     # To execute the query you will need to create the view 'popularauth'
     # (See README - Notes 2)
-    cursor.execute('select * from popularauth;')
-    result_popular_authors = cursor.fetchall()
-    conn.close()
-
     # Loops over the dict and prints the most popular books
-    for authors in result_popular_authors:
+    for authors in query_exec('select * from popularauth;'):
         print('Author: "{}" with {} views'.format(authors[1], authors[0]))
 
 
@@ -57,43 +61,22 @@ def error_day():
     Function that connects to the newsdata sql database and returns
     the days with more than 1% of requests that lead to errors:.
     '''
-    # Connects to database news
-    conn = psycopg2.connect("dbname=news")
-    cursor = conn.cursor()
-
-    # Modify the query so you can have only the date and not the time:
-    # view 'normal_date' (README 1.4)
-
-    # Query that gets the ammount of requests per day:
-    cursor.execute("select count(*), day from normal_date group by day;")
-    total_requests = cursor.fetchall()
-
-    # Query that gets the ammount of error requests per day:
-    cursor.execute("select count(*), day from normal_date "
-                   "where status = '404 NOT FOUND' group by day order by day;")
-    error_requests = cursor.fetchall()
-
-    conn.close()
-
+    # To execute the query you will need to create the view 'status'
+    # (See README - Notes 3)
     # Loop to check if the 404 errors were more than 1%
-    # This will loop in all 31 days of the log
-    i = 0
-    for days in total_requests:
-        # Checking if the 404 status are more than 1% in each day
-        if error_requests[i][0] > (days[0] * .01):
-            # If the statement is true assign a variable on
-            # how much the percentage was actually that day.
-            error_per = (error_requests[i][0]*100) / (days[0])
-            print("{} - {:0.2f}% of error requests".format(days[1], error_per))
-        i += 1
+    for date in query_exec('select * from status;'):
+        if (date[1] * .01) < date[2]:
+            print("{} - {:0.2f}% of error"
+                  "requests".format(date[0], (date[2]*100) / (date[1])))
 
-print()
-print("1. The most popular 3 articles of all time are:")
-reporting_3_popular()
-print()
-print("2. The most popular authors are:")
-reporting_popular_authors()
-print()
-print("3. Day(s) with more than 1% of requests that lead to errors:")
-error_day()
-print()
+if __name__ == "__main__":
+    print()
+    print("1. The most popular 3 articles of all time are:")
+    reporting_3_popular()
+    print()
+    print("2. The most popular authors are:")
+    reporting_popular_authors()
+    print()
+    print("3. Day(s) with more than 1% of requests that lead to errors:")
+    error_day()
+    print()
